16ae13a34660def55f372fe572716197b8d09c44
amircodington <amircodington@gmail.com>
2025-08-24 08:11:06 +0330

    working on first plugin

---
+++ plugins/core-gamestore/core-gamestore.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: GameStore General
+ * Description: Core Code for the GameStore theme.
+ * Version: 1.0.0
+ * Author: Amir Codington
+ * Author URI: https://amircodington.com
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: core-gamestore
+ * Domain Path: /languages
+ */
+define('GAMESTORE_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('GAMESTORE_PLUGIN_PATH', plugin_dir_path(__FILE__));
